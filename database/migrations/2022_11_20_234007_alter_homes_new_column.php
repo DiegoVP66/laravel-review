a2807402a41ced79359601d80d8b587d1545b81d
diegovp66 <dvpdev33@gmail.com>
2022-11-20 20:48:27 -0300

    php artisan migrate:rollback

--- database/migrations/2022_11_20_234007_alter_homes_new_column.php
+++ database/migrations/2022_11_20_234007_alter_homes_new_column.php
@@ -25,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('homes',function(Blueprint $table){
+            $table->dropColumn('age');
+        });
     }
 };
