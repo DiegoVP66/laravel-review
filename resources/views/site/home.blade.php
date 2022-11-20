35a1486159860b2247f680c3486a9a3523d4ed59
diegovp66 <dvpdev33@gmail.com>
2022-11-20 12:31:46 -0300

    Add blade layout, section, extends

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-        <h1>Hello world!</h1>
-</body>
-</html>
+@extends('site.layouts.basic')
+@section('title', $title)
+@section('content')
+
+    <div>
+        <h1> Hello World! </h1>
+    </div>
+
+@endsection
