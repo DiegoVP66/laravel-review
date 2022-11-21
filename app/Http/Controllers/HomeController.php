b266140a315ac34c4cf448be9325823fb3fea5cf
diegovp66 <dvpdev33@gmail.com>
2022-11-21 19:50:02 -0300

    Insert data into db input Request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Home;
 class HomeController extends Controller
 {
     public function home(Request $request){
-        echo '<pre>';
+       /* echo '<pre>';
         print_r($request->all());
         echo '</pre>';
         echo $request->input('name');
-        return view('site.home', ['title' => 'Home Test']);
+        */
+        
+        $person = new Home();
+        
+            $person->name = $request->input('name');
+            $person->age = $request->input('age');     
+        
+        print_r($person->getAttributes());
+       
+       return view('site.home', ['title' => 'Home Test']);
+       
     }
 }
--- resources/views/site/layouts/_components/form.blade.php
+++ resources/views/site/layouts/_components/form.blade.php
@@ -1,6 +1,7 @@
 
 <form action={{route('site.home')}} method="post">
     @csrf
-    <input type="text" name="name" placeholder="name">
+    <input type="text" name="name" placeholder="name" required>
+    <input type="text" name="age" placeholder="age" required>
     <button type="submit">send</button>
 </form>
